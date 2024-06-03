8b081b1f7cc5f4d9d43bf4cdccec2cf92cb65dcf
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-06-03 03:49:55 +0000

    Criação do endpoint' '/oi' & criação do script 'start'

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000)
+
+app.get('/oi', function(req, res) {
+    res.send('Óla mundo!')
+})
+
+app.get('/2', function(){
+    console.log('Aplicação rodando em http://localhost:3000')
+})
